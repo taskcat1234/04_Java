5f9a0669ea25194638db65cf9a2056f2865abd88
taskcat1234 <1232168@naver.com>
2025-01-20 11:32:59 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,26 @@
+// JS와 주석 동일
+
+/* Run 버튼 (단축키 ctrl+f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.java) -> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후  .class 파일 실행 (java class파일명)
+*/
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이클립스 실행 중");
+		System.out.println("자동완성 불편해");
+		System.out.println("자동완성 서비스 고처줘");
+		/* Java API DOC
+		 *  Java에서 제공해주는 코드의 사용법이 설명 되어있는 문서
+		 * 
+		 * DOC 확인 방법
+		 * 1) 구글에서 java 21 api 검색
+		 * 
+		 * 2) 궁금한 코드 클릭 -> shift +f2
+		 */
+
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,14 @@
+public class JavaTest{
+
+  public static void main(String[] args){
+  
+    System.out.println("Hello World!!!");
+    System.out.println("오늘이 금요일 이였으면 좋겠다"); 
+    System.out.println("왜 월요일이지");
+    for(int i =1 ; i <=5 ; i++){
+      System.out.println(i+ "번째 줄 출력");
+      }
+
+  }
+
+}
